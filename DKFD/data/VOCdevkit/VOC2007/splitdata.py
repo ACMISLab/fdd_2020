df50e78d9bfd2bad4392c1ed4c99436e41c30495
荏苒 <74938994+fdd-fbb@users.noreply.github.com>
2023-04-15 21:57:57 +0800

    Update splitdata.py

--- DKFD/data/VOCdevkit/VOC2007/splitdata.py
+++ DKFD/data/VOCdevkit/VOC2007/splitdata.py
@@ -1,15 +1,15 @@
 import os
 import random
 
-xmlfilepath = r'C:\Users\ASUS\Desktop\mmdetection-master - BESR\mmdetection-master\data\VOCdevkit\VOC2007\Annotations'
+xmlfilepath = r'data/VOCdevkit/VOC2007/Annotations'
 
-txtsavepath = r'C:\Users\ASUS\Desktop\ppdet-2.2-BESRGAN\dataset\voc\VOCdevkit\VOC2012\ImageSets\Main'
+txtsavepath = r'data/VOCdevkit/VOC2007/ImageSets/Main'
 total_xml = os.listdir(xmlfilepath)
 random.shuffle(total_xml)
 print(len(total_xml))
 
 #提取原始测试集的所有图像名称
-with open(r'C:\Users\ASUS\Desktop\mmdetection-master - BESR\mmdetection-master\data\VOCdevkit\VOC2007\ImageSets\Main\test.txt','r') as f:
+with open(r'C:data/VOCdevkit/VOC2007/ImageSets/Main/test.txt','r') as f:
     testlist=f.read().splitlines()#读取每一行不带\n
 print(len(testlist))
 trainval=[]
@@ -20,6 +20,6 @@ for item in total_xml:
 #print(len(testlist))
 #print(len(trainval))
 print(len(trainval))
-with open(r'/data/VOCdevkit/VOC2007/ImageSets/Main/4.11trainval.txt', 'w') as ftrainval:
+with open(r'/data/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt', 'w') as ftrainval:
     for item in trainval:
         ftrainval.write(item+'\n')
